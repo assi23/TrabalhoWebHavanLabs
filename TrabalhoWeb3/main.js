28004430a211bffc4c50b0f88c19b5e2d04c81ea
assi23 <guiassi@hotmail.com>
2021-07-28 17:08:56 -0300

    criação do crud e add tr automatico

---
+++ TrabalhoWeb3/main.js
@@ -0,0 +1,35 @@
+var bandas = [
+                //dicionario
+                {"id":1,"nome":"Black Sabbath","gen":"Rock","pais":"EUA"},
+                {"id":2,"nome":"Iron Maiden","gen":"Rock","pais":"EUA"}
+            ]
+
+function cadastrar(){
+    let id = parseInt(document.getElementById("id").value);
+    let nome = document.getElementById("nome").value;
+    let gen = document.getElementById("gen").value;
+    let pais = document.getElementById("pais").value;
+    bandaCadastradas = {"id":id,"nome":nome,"gen":gen,"pais":pais};
+    bandas.push(bandaCadastradas);
+    carregaTabela();
+    limpar(); 
+}
+
+function carregaTabela(){
+    let tbody = document.getElementsByTagName("tbody")[0];
+    tbody.innerHTML = '';
+    bandas.forEach(x =>{
+        tbody.innerHTML += `<tr><td>${x['id']}</td> <td>${x['nome']}</td>  <td>${x['gen']}</td> <td>${x['pais']}</td></tr>`;
+    });
+}
+
+function limpar(){
+    document.getElementById("id").value ='';
+    document.getElementById("nome").value ='';
+    document.getElementById("gen").value ='';
+    document.getElementById("pais").value ='';
+}
+
+window.onload = function(){
+    carregaTabela();
+}
